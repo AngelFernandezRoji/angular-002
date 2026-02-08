81a5f194834d7d7a649f1e0f6f4be180b306b0c1
Angel Fernandez <angelfernandezroji.dev@gmail.com>
2026-02-08 10:38:45 +0100

    Pequeños cambios (no importantes)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from './models/post';
+import { PostsService } from './services/posts';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, AsyncPipe, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('posts-get');
+export class AppComponent {
+   posts$: Observable<Post[]>;
+
+  constructor(private postsService: PostsService) {
+    this.posts$ = this.postsService.getAll();
+  }
 }
